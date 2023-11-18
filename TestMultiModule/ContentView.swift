3ce01154be8701bf1f5162ebc452bb73f853f76a
杉岡成哉 <a1209s518n928@gmail.com>
2023-11-18 12:17:13 +0900

    Initial Commit

---
+++ TestMultiModule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestMultiModule
+//
+//  Created by 杉岡成哉 on 2023/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestMultiModule/TestMultiModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMultiModuleApp.swift
+//  TestMultiModule
+//
+//  Created by 杉岡成哉 on 2023/11/18.
+//
+
+import SwiftUI
+
+@main
+struct TestMultiModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
